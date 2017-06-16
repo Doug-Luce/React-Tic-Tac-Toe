689d96efedf0383b492b1e5fdb53b8c9200bd8c8
Doug Luce <dluce@entercom.com>
2017-06-16 09:59:13 -0700

    Finished code for successfully checking for a winner. I had to extend the native array object to add a equals method to compare two arrays. It will now log the winner.

--- src/Game.js
+++ src/Game.js
@@ -131,9 +131,13 @@ class Game extends Component {
     for (var i = 0; i < winStates.length; i++) {
       for (var y = 0; y < winStates[i].length; y++) {
         console.log(winStates[i][y]);
+        if(winStates[i][y].equals(['X','X','X'])) {
+          console.log('X WINS!');
+        } else if (winStates[i][y].equals(['O','O','O'])){
+          console.log('O WINS!');
+        }
       }
       console.log('---------------------');
-      
     }
 
 
@@ -148,3 +152,33 @@ class Game extends Component {
 
 
 export default Game;
+
+// Warn if overriding existing method
+if(Array.prototype.equals)
+    console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
+// attach the .equals method to Array's prototype to call it on any array
+Array.prototype.equals = function (array) {
+    // if the other array is a falsy value, return
+    if (!array)
+        return false;
+
+    // compare lengths - can save a lot of time 
+    if (this.length !== array.length)
+        return false;
+
+    for (var i = 0, l=this.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!this[i].equals(array[i]))
+                return false;       
+        }           
+        else if (this[i] !== array[i]) { 
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;   
+        }           
+    }       
+    return true;
+}
+// Hide method from for-in loops
+Object.defineProperty(Array.prototype, "equals", {enumerable: false});
